4431f63c41e53961dbc307ec287588a9228ae8d4
Pablo Hoffman <pablo@pablohoffman.com>
2012-10-09 11:18:51 -0200

    scrapylogs/scrapyitems: use SHUB_STORAGE env.var instead of scrapy setting

--- hubstorage/scrapyitems.py
+++ hubstorage/scrapyitems.py
@@ -7,21 +7,17 @@ from hubstorage import Client
 class HubStorage(object):
     """Extension to write scraped items to HubStorage"""
 
-    def __init__(self, url):
+    def __init__(self):
         if 'SHUB_JOB' not in os.environ:
             raise NotConfigured
         apikey = os.environ['SHUB_JOBAUTH']
-        client = Client(apikey, url=url)
+        apiurl = os.environ['SHUB_STORAGE']
+        client = Client(apikey, url=apiurl)
         path = "/items/%(SHUB_PROJECT)s/%(SHUB_SPIDER)s/%(SHUB_JOB)s" % os.environ
         self.writer = client.open_item_writer(path)
-        log.msg("HubStorage: writing items to %s" % urlparse.urljoin(url, path))
+        log.msg("HubStorage: writing items to %s" % urlparse.urljoin(apiurl, path))
         dispatcher.connect(self.item_scraped, signals.item_scraped)
         dispatcher.connect(self.spider_closed, signals.spider_closed)
-
-    @classmethod
-    def from_settings(cls, settings):
-        url = settings.get('SHUB_STORAGE', 'http://localhost:8002')
-        return cls(url)
 
     def item_scraped(self, item, spider):
         item = dict(item)
