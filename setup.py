60d324e65b801e9b916f061d8a6100862e10b086
Daniel Graña <dangra@gmail.com>
2012-12-03 21:12:51 -0200

    bump version to 0.3

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='hubstorage',
-      version='0.2',
+      version='0.3',
       license='BSD',
       description='Client interface for Scrapinghub HubStorage',
       author='Scrapinghub',
