e6067d47c0200968b7c24eed0ab9b7e6a00f99c8
Daniel Graña <dangra@gmail.com>
2013-01-16 04:12:02 -0200

    update library to state changes trough jobq

--- hubstorage/jobq.py
+++ hubstorage/jobq.py
@@ -11,7 +11,26 @@ class JobQ(ResourceType):
         r = self.apipost('push', jl=jobparams)
         return r.next()
 
-    def summary(self, _queuename=None):
-        path = urlpathjoin('summary', _queuename)
-        r = self.apiget(path)
-        return r.next() if _queuename else r
+    def summary(self, _queuename=None, spiderid=None):
+        path = urlpathjoin(spiderid, 'summary', _queuename)
+        r = list(self.apiget(path))
+        return (r and r[0] or None) if _queuename else r
+
+    def _set_state(self, job, state):
+        if isinstance(job, dict):
+            key = job['key']
+        elif hasattr(job, 'key'):
+            key = job.key
+        else:
+            key = job
+        r = self.apipost('update', jl={'key': key, 'state': state})
+        return r.next()
+
+    def start(self, job):
+        return self._set_state(job, 'running')
+
+    def finish(self, job):
+        return self._set_state(job, 'finished')
+
+    def delete(self, job):
+        return self._set_state(job, 'deleted')
---
+++ tests/test_system.py
@@ -0,0 +1,107 @@
+from hstestcase import HSTestCase
+from hubstorage import HubstorageClient
+
+
+class SystemTest(HSTestCase):
+
+    MAGICN = 1211
+
+    def setUp(self):
+        super(HSTestCase, self).setUp()
+        endpoint = self.hsclient.endpoint
+        # Panel - no client auth, only project auth using user auth token
+        self.panelclient = HubstorageClient(endpoint=endpoint)
+        self.panelproject = self.panelclient.get_project(self.projectid, auth=self.auth)
+        # Runner - client uses global auth to poll jobq
+        self.runnerclient = HubstorageClient(endpoint=endpoint, auth=self.auth)
+        # Scraper - uses job level auth, no global or project auth available
+        self.scraperclient = HubstorageClient(endpoint=endpoint)
+
+    def test_succeed_with_close_reason(self):
+        return
+        p = self.panelproject
+        pushed = p.jobq.push(self.spidername)
+        # check pending state
+        job = p.get_jobs(self.spiderid).next()
+        self.assertEqual(job.metadata.get('state'), 'pending')
+        # consume msg from runner
+        self._run_runner(pushed, close_reason='all-good')
+        # query again from panel
+        job = p.get_jobs(self.spiderid).next()
+        self.assertEqual(job.metadata.get('state'), 'finished')
+        self.assertEqual(job.metadata.get('close_reason'), 'all-good')
+
+    def test_succeed_without_close_reason(self):
+        return
+        p = self.panelproject
+        pushed = p.jobq.push(self.spidername)
+        # check pending state
+        job = p.get_jobs(self.spiderid).next()
+        self.assertEqual(job.metadata.get('state'), 'pending')
+        # consume msg from runner
+        self._run_runner(pushed, close_reason=None)
+        # query again from panel
+        job = p.get_jobs(self.spiderid).next()
+        self.assertEqual(job.metadata.get('state'), 'finished')
+        self.assertEqual(job.metadata.get('close_reason'), 'no_reason')
+
+    def test_scraper_failure(self):
+        p = self.panelproject
+        pushed = p.jobq.push(self.spidername)
+        # check pending state
+        job = p.get_jobs(self.spiderid).next()
+        self.assertEqual(job.metadata.get('state'), 'pending')
+        # consume msg from runner
+        self._run_runner(pushed, close_reason=IOError('no more resources, ha!'))
+        # query again from panel
+        job = p.get_jobs(self.spiderid).next()
+        self.assertEqual(job.metadata.get('state'), 'finished')
+        self.assertEqual(job.metadata.get('close_reason'), 'failed')
+        # MAGICN per log level messages plus one of last failure
+        stats = job.logs.stats()
+        self.assertTrue(stats)
+        self.assertEqual(stats['totals']['input_values'], self.MAGICN * 4 + 1)
+
+    def _run_runner(self, pushed, close_reason):
+        # TODO: poll for job using jobq
+        polled = pushed
+        jobkey = polled['key']
+        jobauth = (jobkey, polled['auth'])
+        job = self.runnerclient.get_job(jobkey)
+        job.started()  # FIXME: remove me and set started_time at HS side
+        self.assertFalse(job.metadata.get('stop_requested'))
+        job.metadata.update(host='localhost', slot=1)
+        self.assertEqual(job.metadata.get('state'), 'running')
+        # run scraper
+        try:
+            self._run_scraper(jobkey, jobauth, close_reason=close_reason)
+        except Exception as exc:
+            job.failed(message=str(exc))
+            # logging from runner must append and never remove messages logged
+            # by scraper
+            self.assertTrue(job.logs.batch_append)
+        else:
+            job.finished()
+
+        self.runnerclient.close()
+
+    def _run_scraper(self, jobkey, jobauth, close_reason=None):
+        job = self.scraperclient.get_job(jobkey, auth=jobauth)
+        for idx in xrange(self.MAGICN):
+            job.items.write({'uuid': idx})
+            job.logs.debug('log debug %s' % idx, idx=idx)
+            job.logs.info('log info %s' % idx, idx=idx)
+            job.logs.warn('log warn %s' % idx, idx=idx)
+            job.logs.error('log error %s' % idx, idx=idx)
+            job.samples.write([idx, idx, idx])
+
+        if isinstance(close_reason, Exception):
+            self.scraperclient.close()
+            raise close_reason
+
+        if close_reason:
+            job.metadata['close_reason'] = close_reason
+
+        job.metadata.save()
+        self.scraperclient.close()
+        del self.scraperclient
