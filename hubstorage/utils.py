b064f528349e4cba4eab254d31b0ec6827dd0ecc
Andres Portillo <andres.portillo@gmail.com>
2013-05-29 15:49:18 -0500

    Remove custom chunks function, it is not longer used

--- hubstorage/utils.py
+++ hubstorage/utils.py
@@ -110,10 +110,3 @@ class iterqueue(object):
                 self.count += 1
             except Empty:
                 break
-
-
-def chunks(l, n):
-    """ Yield successive n-sized chunks from l.
-    """
-    for i in xrange(0, len(l), n):
-        yield l[i:i + n]
