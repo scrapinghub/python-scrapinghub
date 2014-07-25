b8882fe5789ed90758ec0ebe1c2db8bc7cb78b4b
Daniel Graña <dangra@gmail.com>
2014-07-25 16:22:29 -0300

    Update setup classifiers

--- setup.py
+++ setup.py
@@ -5,18 +5,29 @@ except ImportError:
     from distutils.core import setup
 
 
-setup(name='scrapinghub',
-      version=__version__,
-      license='BSD',
-      description='Client interface for Scrapinghub API',
-      author='Scrapinghub',
-      author_email='info@scrapinghub.com',
-      url='http://github.com/scrapinghub/python-scrapinghub',
-      platforms = ['Any'],
-      py_modules = ['scrapinghub'],
-      install_requires = ['requests'],
-      classifiers = [ 'Development Status :: 4 - Beta',
-                      'License :: OSI Approved :: BSD License',
-                      'Operating System :: OS Independent',
-                      'Programming Language :: Python']
+setup(
+    name='scrapinghub',
+    version=__version__,
+    license='BSD',
+    description='Client interface for Scrapinghub API',
+    author='Scrapinghub',
+    author_email='info@scrapinghub.com',
+    url='http://github.com/scrapinghub/python-scrapinghub',
+    platforms = ['Any'],
+    py_modules = ['scrapinghub'],
+    install_requires = ['requests'],
+    classifiers = [
+        'Development Status :: 5 - Production/Stable',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Topic :: Internet :: WWW/HTTP',
+    ],
 )
