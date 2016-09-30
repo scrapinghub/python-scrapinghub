450f8cd9fd62d85fbc40f488e74fe7da7a73b15a
Viktor Shlapakov <vshlapakov@gmail.com>
2016-09-30 12:51:36 +0300

    Sort out other files, update common files

--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
+import sys
 from scrapinghub import __version__
 try:
     from setuptools import setup
 except ImportError:
     from distutils.core import setup
 
+# We can't import hubstorage.__version__ because it imports "requests" and it
+# can not be available yet
+is_pypy = '__pypy__' in sys.builtin_module_names
+mpack_required = 'msgpack-pypy>=0.0.2' if is_pypy else 'msgpack-python>=0.4.7'
 
 setup(
     name='scrapinghub',
@@ -14,8 +19,9 @@ setup(
     author_email='info@scrapinghub.com',
     url='http://github.com/scrapinghub/python-scrapinghub',
     platforms = ['Any'],
-    py_modules = ['scrapinghub'],
-    install_requires = ['requests'],
+    py_modules = ['scrapinghub', 'hubstorage'],
+    install_requires=['requests', 'retrying>=1.3.3', 'six>=1.10.0'],
+    extras_require = {'msgpack': [mpack_required]},
     classifiers = [
         'Development Status :: 5 - Production/Stable',
         'License :: OSI Approved :: BSD License',
