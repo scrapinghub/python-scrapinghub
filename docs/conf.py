1ba0d23a302d5d38921617b028f3e9f87ce22a24
Alexander Chekunkov <chekunkov@gmail.com>
2017-03-29 12:41:22 +0100

    Always use Sphinx Read The Docs theme

--- docs/conf.py
+++ docs/conf.py
@@ -166,20 +166,11 @@ texinfo_documents = [
      'Miscellaneous'),
 ]
 
-# Following is taken from https://github.com/snide/sphinx_rtd_theme#
-#   using-this-theme-locally-then-building-on-read-the-docs
+# Set Sphinx Read The Docs theme
+import sphinx_rtd_theme
 
-# on_rtd is whether we are on readthedocs.org,
-# this line of code grabbed from docs.readthedocs.org
-
-on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
-
-if not on_rtd:  # only import and set the theme if we're building docs locally
-    import sphinx_rtd_theme
-    html_theme = 'sphinx_rtd_theme'
-    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
-
-# otherwise, readthedocs.org uses their theme by default, no need to specify it
+html_theme = 'sphinx_rtd_theme'
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # disable cross-reference for ivar
 # patch taken from http://stackoverflow.com/a/41184353/1932023
