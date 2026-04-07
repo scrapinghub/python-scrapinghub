c06d8bc22b7da6c05d9cbe2e544e05547a9eddca
Adrian <adrian@zyte.com>
2026-04-07 11:15:00 +0200

    Add llms.txt and llms-full.txt generation (#188)

--- docs/conf.py
+++ docs/conf.py
@@ -39,7 +39,7 @@ VERSION = __version__.rsplit('.', 2)[0]
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc']
+extensions = ['sphinx_scrapy']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -160,9 +160,3 @@ texinfo_documents = [
      author, 'python-scrapinghub', 'One line description of project.',
      'Miscellaneous'),
 ]
-
-# Set Sphinx Read The Docs theme
-import sphinx_rtd_theme
-
-html_theme = 'sphinx_rtd_theme'
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
